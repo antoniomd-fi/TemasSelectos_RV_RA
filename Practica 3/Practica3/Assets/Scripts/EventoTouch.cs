e2a7f7c16fd9c0c667fd0d5319f4affecacd13cc
antoniomd-fi <90221784+antoniomd-fi@users.noreply.github.com>
2022-03-12 20:45:15 -0600

    Práctica 3

---
+++ Practica 3/Practica3/Assets/Scripts/EventoTouch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EventoTouch : MonoBehaviour
+{
+   
+   Renderer esfera;
+    public Texture[] texturas;
+
+    private int i=0;
+    // Start is called befre the first frame update
+    void Start()
+    {
+        esfera = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touches.Length == 1)
+        {
+            if (i == 2)
+            {
+                i = 0;
+                esfera.material.SetTexture("_MainTex", texturas[i]);
+            }
+            else
+            {
+                i++;
+                esfera.material.SetTexture("_MainTex", texturas[i]);
+            }
+        }
+    }
+}
